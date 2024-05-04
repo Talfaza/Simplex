67313fb81b590ed6785911570de1acaa4628d40f
Talfaza <charifyahia4@gmail.com>
2024-05-04 16:19:48 +0100

    new: getting data from user

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import numpy as np
+import re
 
-x1 = None
-x2 = None
-x3 = None
+nbrVar = int(input("Donner le nombre de variables : "))
+
+variables = {}
+for i in range(nbrVar):
+    variable_name = f"x{i+1}"
+    while True:
+        try:
+            variable_value = int(input(f"Donner la valeur de x{i + 1}: "))
+            variables[variable_name] = variable_value
+            break
+        except ValueError:
+            print("Veuillez entrer uniquement des nombres entiers.")
+
+nbrContraint = int(input("Donner le nombre de Contraint : "))
+
+constraints = []
+for i in range(nbrContraint):
+    constraint_str = input(f"Entrez la contrainte {i + 1}: ")
+    constraints.append(constraint_str)
+
